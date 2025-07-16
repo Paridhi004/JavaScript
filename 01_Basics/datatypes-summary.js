b28f41b871cd506fd29f88251fc1d0f688ee888f
Paridhi004 <145322115+Paridhi004@users.noreply.github.com>
2025-07-16 14:03:23 +0000

    Memory allocation

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -85,6 +85,12 @@ console.log(typeof anotherId);
 //        Function  =>  function (object function)
 //        Object  =>  object
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//datatypes are based on memory allocation
+// Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+// Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
 
 
 
