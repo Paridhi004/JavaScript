41916d6b73fb11397d85e5ea0a9688b2ed41303e
Paridhi004 <145322115+Paridhi004@users.noreply.github.com>
2025-07-18 16:27:21 +0000

    Strings Done

---
+++ 01_Basics/05_StringandItsFunc.js
@@ -0,0 +1,15 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // string interpolation -> Best Method to concatenate string.
+
+const gameName = new String('hitesh-hc-com') // another way to declare strings -> using new keyword.
+
+ console.log(gameName[0]); // to access the character present at index 0.
+
+ // console.log(gameName.__proto__);
+
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
