2932146cfa901cd955701d9d94f8cc2c1d68c26c
Paridhi Rastogi <paridhirastogi85@gmail.com>
2025-08-11 20:14:35 +0530

    Initial Commit

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,49 @@
+function sayMyName(){  // we've created a function.
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+//sayMyName // reference of function
+//sayMyName() // execution of function.
+
+function addTwoNumbers(number1 , number2){
+     console.log(number1 + number2);
+     
+}
+
+addTwoNumbers(3 , 4) // 7
+addTwoNumbers(3 , "4") // 34
+
+// concept of Arguments and parameters:
+// 1. Arguments -> 3 , 4 are arguments. Values passes during function call
+//2. Parameters -> number1 , number2 are parameters. Values in the defination of the function.  
+
+
+// What if I want to save the output from the function?
+
+function addTwoNumbers(number1 , number2){
+    //  let result = number1 + number2
+    //  return result // Note-> Return statement k baad kuch bhi execute ni hoga
+
+    return number1 + number2
+}
+const result = addTwoNumbers(3 , 4)
+console.log(result);
+
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Please enter a username!");
+        return
+    }
+    return `${username} just logged in `
+}
+console.log(loginUserMessage("Paridhi")); // Paridhi just logged in
+console.log(loginUserMessage("")); // just logged in
+console.log(loginUserMessage()); // undefined just logged in -> for this check the condition.
+
+
